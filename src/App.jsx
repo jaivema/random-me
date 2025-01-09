1f0f396effe981142e774db64e9c66e41754a04d
jverdejo <jverdejo@eqtic.net>
2025-01-09 20:07:44 +0100

    improvement filters and retro style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import Sidebar from './components/Sidebar'
 
 function App() {
-  const [numberUsers, setNumberUsers] = useState(3)
+  const [numberUsers, setNumberUsers] = useState(4)
   const [randomUsers, setRandomUsers] = useState([])
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
@@ -55,27 +55,27 @@ function App() {
             {isLoading && <p className='onLoading'>Loading...</p>}
             {error ? <p className='onError'><strong>{error}</strong></p> : randomUsers.map((user, index) => (
               <article key={user.login.uuid}>
-              <h3>{user.name.first} {user.name.last}</h3>
-              <img src={user.picture.medium} alt={`${user.name.first} ${user.name.last}`} />
-              <p>Country: {user.location.country} {user.nat}</p>
-              <p>State: {user.location.state}</p>
-              <p>City: {user.location.city}</p>
-              <p>Email: {user.email}</p>
-            </article>
+                <h3>{user.name.first} {user.name.last}</h3>
+                <img src={user.picture.medium} alt={`${user.name.first} ${user.name.last}`} />
+                <p>Country: {user.location.country} {user.nat}</p>
+                <p>State: {user.location.state}</p>
+                <p>City: {user.location.city}</p>
+                <p>Email: {user.email}</p>
+              </article>
             ))}
           </section>
         </main>
       </div>
-        <footer>
-          <h3>Powered by <a href="https://randomuser.me">https://randomuser.me</a></h3>
-          <h4>Copyright Notice</h4>
-          <p>
-            All randomly generated photos were hand picked from the authorized section of
-            <a href="http://uifaces.com"> UI Faces</a>. Please visit
-            <a href="https://web.archive.org/web/20160811185628/http://uifaces.com/faq"> UI Faces FAQ </a>
-            for more information regarding how you can use these faces.
-          </p>
-        </footer>
+      <footer>
+        <h3>Powered by <a href="https://randomuser.me">https://randomuser.me</a></h3>
+        <h4>Copyright Notice</h4>
+        <p>
+          All randomly generated photos were hand picked from the authorized section of
+          <a href="http://uifaces.com"> UI Faces</a>. Please visit
+          <a href="https://web.archive.org/web/20160811185628/http://uifaces.com/faq"> UI Faces FAQ </a>
+          for more information regarding how you can use these faces.
+        </p>
+      </footer>
     </div>
   )
 }
